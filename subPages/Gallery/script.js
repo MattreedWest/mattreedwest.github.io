671d1e5542c136d3e96ed72cc16f9d4ce52c772a
MattreedWest <ligioner86@inbox.ru>
2020-11-07 19:56:53 +0700

    corrected

--- subPages/Gallery/script.js
+++ subPages/Gallery/script.js
@@ -1,3 +1,9 @@
+let arrImg = document.querySelectorAll("#imgSet img");
+let carouselImg = document.querySelectorAll(".carousel-inner>.carousel-item");
 $('.carousel').carousel ({
     interval:2000
-})
+});
+
+$(arrImg).click(function () { 
+    $(".carouselMain").carousel(document.querySelectorAll("#imgSet img"))
+});
